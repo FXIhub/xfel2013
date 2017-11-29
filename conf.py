8005a501f5e238f3fdc3dc97038eb5bba6e4ef78
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2017-11-29 10:38:50 +0100

    Updated scripts and config

--- conf.py
+++ conf.py
@@ -4,30 +4,30 @@ import analysis.agipd
 
 # Reading from raw AGIPD data source
 #agipd_socket = 'tcp://10.253.0.51:4500'
-#agipd_socket = 'tcp://127.0.0.1:4500'
-#agipd_key = 'SPB_DET_AGIPD1M-1/DET'
-#agipd_format = 'combined'
+agipd_socket = 'tcp://127.0.0.1:4500'
+agipd_key = 'SPB_DET_AGIPD1M-1/DET'
+agipd_format = 'combined'
 
 # Reading from calibrated AGIPD data source
 #agipd_socket = 'tcp://10.253.0.51:4501'
 #agipd_key = 'SPB_DET_AGIPD1M-1/DET'
 #agipd_format = 'combined'
 
-# Reading from individual raw AGIPD data source (panel 03)
-#agipd_socket = 'tcp://10.253.0.52:4600'
-agipd_socket = 'tcp://127.0.0.1:4600'
-agipd_key = 'SPB_DET_AGIPD1M-1/DET/3CH0:xtdf'
-agipd_format = 'panel'
+# # Reading from individual raw AGIPD data source (panel 03)
+# agipd_socket = 'tcp://10.253.0.52:4600'
+# # agipd_socket = 'tcp://127.0.0.1:4600'
+# agipd_key = 'SPB_DET_AGIPD1M-1/DET/3CH0:xtdf'
+# agipd_format = 'panel'
 
 # Reading from individual raw AGIPD data source (panel 04)
 #agipd_socket = 'tcp://10.253.0.52:4601'
 #agipd_key = 'SPB_DET_AGIPD1M-1/DET/4CH0:xtdf'
 #agipd_format = 'panel'
 
-# Reading from individual raw AGIPD data source (panel 15)
-#agipd_socket = 'tcp://10.253.0.52:4602'
-#agipd_key = 'SPB_DET_AGIPD1M-1/DET/15CH0:xtdf'
-#agipd_format = 'panel'
+# # Reading from individual raw AGIPD data source (panel 15)
+# agipd_socket = 'tcp://10.253.0.52:4602'
+# agipd_key = 'SPB_DET_AGIPD1M-1/DET/15CH0:xtdf'
+# agipd_format = 'panel'
 
 state = {}
 state['Facility'] = 'euxfel'
