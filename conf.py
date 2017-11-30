78593e40a54e39f335d9b281021b7e188075e627
hantke@xray.bmc.uu.se <hantke@xray.bmc.uu.se>
2017-11-30 16:39:19 +0100

    Karabo defines the cellId differently than CFEL - this is now taken care of also in the simulated example

--- conf.py
+++ conf.py
@@ -10,16 +10,17 @@ do_offline = True
 
 cellId_allowed_range = range(0, 30)
 
-#agipd_format = 'combined'
-agipd_format = 'panel'
+agipd_format = 'combined'
+#agipd_format = 'panel'
 
 # For the combined format precalibrated data can be selected from Karabo
 do_precalibrate = False
-do_calibrate = not do_precalibrate
+#do_calibrate = not do_precalibrate
+do_calibrate = True
 
 # Apply geometry (only effective if agipd_format='combined')
-#do_assemble = True
-do_assemble = False
+do_assemble = True
+#do_assemble = False
 
 # The central 3 working panels have the IDs 3, 4, 15
 agipd_panel = 3
@@ -89,11 +90,15 @@ analysis.agipd.init_geom(filename=fn_agipd_geom)
 
 def onEvent(evt):
 
-    cellId = evt['eventID']['Timestamp'].cellId
+    native_cellId = evt['eventID']['Timestamp'].cellId
+    
+    cellId = native_cellId // 2 - 1
     pulseId = evt['eventID']['Timestamp'].pulseId
     if cellId not in cellId_allowed_range:
         print("WARNING: Skip event pulseId=%i. cellId=%i out of allowed range." %  (pulseId, cellId))
         return
+    else:
+        print("pulseId=%i\tcellId=%i" %  (pulseId, cellId))
     
     # Available keys
     #print("Available keys: " + str(evt.keys()))
@@ -105,9 +110,11 @@ def onEvent(evt):
     agipd_data = analysis.agipd.getAGIPD(evt, evt['photonPixelDetectors'][agipd_key],
                                          cellID=cellId, panelID=agipd_panel,
                                          calibrate=do_calibrate, assemble=do_assemble)
+    print(agipd_data.data.mean())
     
     # Plotting the AGIPD panel
     plotting.image.plotImage(agipd_data)
+    #plotting.image.plotImage(evt['photonPixelDetectors'][agipd_key])
 
     # TODO: Add more ......
     # ....
