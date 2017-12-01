248c6fe8c3e6b890e446cdc8c705aae28d04cd05
hantke@xray.bmc.uu.se <hantke@xray.bmc.uu.se>
2017-12-01 17:46:51 +0100

    Clean up and take out preamble from conf.py import it from new conf_xfel2013.py.

--- conf.py
+++ conf.py
@@ -2,6 +2,8 @@ import os
 import plotting.image
 import analysis.agipd
 
+from conf_xfel2013 import *
+
 do_offline = True
 
 # =================== #
@@ -13,6 +15,11 @@ cellId_allowed_range = range(0, 30)
 agipd_format = 'combined'
 #agipd_format = 'panel'
 
+# The central 3 working panels have the IDs 3, 4, 15
+agipd_panel = 3
+#agipd_panel = 4
+#agipd_panel = 15
+
 # For the combined format precalibrated data can be selected from Karabo
 do_precalibrate = False
 #do_calibrate = not do_precalibrate
@@ -22,40 +29,7 @@ do_calibrate = True
 do_assemble = True
 #do_assemble = False
 
-# The central 3 working panels have the IDs 3, 4, 15
-agipd_panel = 3
-#agipd_panel = 4
-#agipd_panel = 15
-
-# Determine the data source
-if do_offline:
-    tcp_prefix = 'tcp://127.0.0.1'
-else:
-    if agipd_format == 'panel':
-        tcp_prefix = 'tcp://10.253.0.52'
-    elif agipd_format == 'combined':
-        tcp_prefix = 'tcp://10.253.0.51'
-if agipd_format == 'panel':
-    # Reading from individual raw AGIPD data source
-    if do_offline:
-        agipd_socket = '%s:4600' % (tcp_prefix)
-    else:
-        agipd_socket = '%s:460%i' % (tcp_prefix, agipd_panel)
-    agipd_key = 'SPB_DET_AGIPD1M-1/DET/3CH0:xtdf'
-elif agipd_format == 'combined':
-    # Reading from raw AGIPD data source
-    if do_precalibrate:
-        agipd_socket = '%s:4501' % tcp_prefix
-    else:
-        agipd_socket = '%s:4500' % tcp_prefix
-    agipd_key = 'SPB_DET_AGIPD1M-1/DET'
-
-# Output calibration to console
-print("AGIPD socket:\t%s" % agipd_socket)
-print("AGIPD key:\t%s" % agipd_key)
-print("AGIPD format: %s" % agipd_format)
-print("AGIPD do assemble: %i" % do_assemble)
-print("AGIPD do calibrate: %i" % do_calibrate)
+agipd_socket, agipd_key = get_agipd_source(agipd_format=agipd_format, agipd_panel=agipd_panel, do_offline=do_offline, do_precalibrate=do_precalibrate)
 
 # =============== #
 # State variables #
@@ -68,21 +42,6 @@ state['euxfel/agipd']['socket'] = agipd_socket
 state['euxfel/agipd']['source'] = agipd_key
 state['euxfel/agipd']['format'] = agipd_format
 
-# ===================== #
-# Read calibration data #
-# ===================== #
-
-this_dir = os.path.dirname(os.path.realpath(__file__))
-
-# Read calibration data (one file per panel)
-calib_dir = "%s/calib" % this_dir
-fn_agipd_calib_list = ['%s/Cheetah-AGIPD%02i-calib.h5' % (calib_dir, panelID) for panelID in range(0, 16)]
-analysis.agipd.init_calib(filenames=fn_agipd_calib_list)
-
-# Read geometry data
-geom_dir = "%s/geometry" % this_dir
-fn_agipd_geom = '%s/agipd_taw9_oy2_1050addu_hmg5.geom' % (geom_dir)
-analysis.agipd.init_geom(filename=fn_agipd_geom)
 
 # ============ #
 # onEvent call #
