b9beb15f1f09cd9d05908ff0109aa8fa442fc231
Kartik Ayyer <kartik.ayyer@desy.de>
2017-12-04 02:25:26 +0100

    calib_from_dark: Now processing all trains in the dark run

--- offline/calib_from_dark.py
+++ offline/calib_from_dark.py
@@ -5,7 +5,7 @@ import sys
 import shutil
 import h5py
 import numpy as np
-import matplotlib.pyplot as plt
+import glob
 
 '''
 Short script to update calibration files with high gain darks using
@@ -20,7 +20,6 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 run = int(sys.argv[1])
-#good_cells = list(range(2,62,2))
 num_cells = 64
 
 folder = '/gpfs/exfel/exp/SPB/201701/p002013/scratch/calib'
@@ -28,24 +27,24 @@ os.makedirs('%s/r%.4d'%(folder, run), exist_ok=True)
 
 for m in range(16):    
     shutil.copyfile('%s/Cheetah-AGIPD%.2d-calib.h5'%(folder, m), '%s/r%.4d/Cheetah-AGIPD%.2d-calib.h5'%(folder, run, m))
-    #dark_sum = np.zeros((len(good_cells),512,128))
     dark_sum = np.zeros((num_cells,512,128))
     
-    dark_file = '/gpfs/exfel/exp/SPB/201701/p002013/raw/r%.4d/RAW-R%.4d-AGIPD%02d-S00001.h5' % (run,run,m)
-    with h5py.File(dark_file, 'r') as f:
-        dset = f['/INSTRUMENT/SPB_DET_AGIPD1M-1/DET/%dCH0:xtdf/image/data' % (m)]    
-        num_trains = dset.shape[0] // num_cells
-        if m == 0:
-            sys.stderr.write('Processing %d trains\n'%num_trains)
-        for i in range(num_trains):
-            #for j in range(len(good_cells)):
-            for j in range(num_cells):
-                index = num_cells*i+j
-                dark_sum[j,:,:] += dset[num_cells*i + j,0,:,:]
-                sys.stderr.write('\r(%.4d, %.4d)' % (i, m))
-        dark_sum /= num_trains
+    #dark_file = '/gpfs/exfel/exp/SPB/201701/p002013/raw/r%.4d/RAW-R%.4d-AGIPD%02d-S00001.h5' % (run,run,m)
+    flist = sorted(glob.glob('/gpfs/exfel/exp/SPB/201701/p002013/raw/r%.4d/RAW-R%.4d-AGIPD%02d-S*.h5' % (run,run,m)))
+    num_trains = 0
+    for fname in flist:
+        with h5py.File(fname, 'r') as f:
+            dset = f['/INSTRUMENT/SPB_DET_AGIPD1M-1/DET/%dCH0:xtdf/image/data' % (m)]    
+            num_trains += dset.shape[0] // num_cells
+            for i in range(dset.shape[0] // num_cells):
+                for j in range(num_cells):
+                    index = num_cells*i+j
+                    dark_sum[j,:,:] += dset[num_cells*i + j,0,:,:]
+                    sys.stderr.write('\r%s: (%.4d, %.4d)' % (os.path.basename(fname), i, m))
+        sys.stderr.write('\n')
+    dark_sum /= num_trains
+    print('Processed %d trains'%num_trains)
 
     with h5py.File('%s/r%.4d/Cheetah-AGIPD%.2d-calib.h5'%(folder, run, m), 'a') as f:
         f['AnalogOffset'][0,:,:,:] = dark_sum
 
-sys.stderr.write('\n')
