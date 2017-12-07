c4a2ab4392a445f9bbd6fbca3d09f82d4343b1a2
Kartik Ayyer <kartik.ayyer@desy.de>
2017-12-07 14:24:27 +0100

    calib_from_dark: Added multi-threading and saving dark offset variance

--- offline/calib_from_dark.py
+++ offline/calib_from_dark.py
@@ -6,12 +6,12 @@ import shutil
 import h5py
 import numpy as np
 import glob
+import multiprocessing as mp
 
 '''
 Short script to update calibration files with high gain darks using
 a specified dark run number.
 
-(Currently only uses 250 trains (251-500) to calculate mean signal)
 Authors: Kartik Ayyer, Filipe Maia
 '''
 
@@ -25,26 +25,36 @@ num_cells = 64
 folder = '/gpfs/exfel/exp/SPB/201701/p002013/scratch/calib'
 os.makedirs('%s/r%.4d'%(folder, run), exist_ok=True)
 
-for m in range(16):    
+def worker(m):
     shutil.copyfile('%s/Cheetah-AGIPD%.2d-calib.h5'%(folder, m), '%s/r%.4d/Cheetah-AGIPD%.2d-calib.h5'%(folder, run, m))
     dark_sum = np.zeros((num_cells,512,128))
+    dark_sumsq = np.zeros((num_cells,512,128))
     
-    #dark_file = '/gpfs/exfel/exp/SPB/201701/p002013/raw/r%.4d/RAW-R%.4d-AGIPD%02d-S00001.h5' % (run,run,m)
     flist = sorted(glob.glob('/gpfs/exfel/exp/SPB/201701/p002013/raw/r%.4d/RAW-R%.4d-AGIPD%02d-S*.h5' % (run,run,m)))
     num_trains = 0
     for fname in flist:
-        with h5py.File(fname, 'r') as f:
-            dset = f['/INSTRUMENT/SPB_DET_AGIPD1M-1/DET/%dCH0:xtdf/image/data' % (m)]    
-            num_trains += dset.shape[0] // num_cells
-            for i in range(dset.shape[0] // num_cells):
-                for j in range(num_cells):
-                    index = num_cells*i+j
-                    dark_sum[j,:,:] += dset[num_cells*i + j,0,:,:]
-                    sys.stderr.write('\r%s: (%.4d, %.4d)' % (os.path.basename(fname), i, m))
-        sys.stderr.write('\n')
+        f = h5py.File(fname, 'r')
+        dset = f['/INSTRUMENT/SPB_DET_AGIPD1M-1/DET/%dCH0:xtdf/image/data' % (m)]    
+        num_trains += dset.shape[0] // num_cells
+
+        for i in range(dset.shape[0] // num_cells):
+            for j in range(num_cells):
+                data = dset[num_cells*i+j,0,:,:].astype('f8')
+                dark_sum[j,:,:] += data[j]
+                dark_sumsq[j,:,:] += data[j]**2
+
+            if m == 0:
+                sys.stderr.write('\r%s: %.4d' % (os.path.basename(fname), i))
+        f.close()
+
     dark_sum /= num_trains
-    print('Processed %d trains'%num_trains)
+    dark_sumsq /= num_trains
 
     with h5py.File('%s/r%.4d/Cheetah-AGIPD%.2d-calib.h5'%(folder, run, m), 'a') as f:
         f['AnalogOffset'][0,:,:,:] = dark_sum
+        f['DarkVariance'] = dark_sumsq - dark_sum**2
+
+pool = mp.Pool(16)
+pool.map(worker, range(16))
+sys.stderr.write('\n')
 
